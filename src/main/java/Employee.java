79a97ae7343e83c25aa2582bca3cb88fcf22f9aa
bob160 <efioesien@gmail.com>
2022-10-09 02:07:55 +0100

    beginning of first project

---
+++ src/main/java/Employee.java
@@ -0,0 +1,46 @@
+public class Employee {
+    public String firstName;
+    public String lastName;
+    public String department;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String setDepartment() {
+        return this.department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
+
+
